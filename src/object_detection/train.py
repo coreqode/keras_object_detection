5260db93e3010edcbc715306c418be9442d6794b
coreqode <chinu.deep100@gmail.com>
2020-04-14 12:42:15 +0000

    training successful

--- src/object_detection/train.py
+++ src/object_detection/train.py
@@ -21,9 +21,9 @@ def train(model, config):
                     baseline=None, restore_best_weights=True
                 )
     
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(
-                config.checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
-                save_weights_only=True, mode='min', save_freq='epoch')
+#    checkpoint = tf.keras.callbacks.ModelCheckpoint(
+ #               config.checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
+  #              save_weights_only=True, mode='min', save_freq='epoch')
     
     train_gen = create_dataset(config.train_batch_size, config.trainset_path, config.shuffle_buffer)
     STEP_SIZE_TRAIN = int((0.95 * config.num_data) // config.train_batch_size)
@@ -45,18 +45,18 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     # hyperparameters
     args.add_argument('--input_shape', type=int, default=224)
-    args.add_argument('--train_batch_size', type=int, default=128)
-    args.add_argument('--val_batch_size', type=int, default=128)
-    args.add_argument('--epochs', type=int, default=100)
-    args.add_argument('--learning_rate', type=int, default=0.01)
-    args.add_argument('--num_data', type=int, default=306452)
-    args.add_argument('--shuffle_buffer', type=int, default=1024)
+    args.add_argument('--train_batch_size', type=int, default=32 * 8)
+    args.add_argument('--val_batch_size', type=int, default=32 * 8)
+    args.add_argument('--epochs', type=int, default= 2)
+    args.add_argument('--learning_rate', type=int, default=0.045)
+    args.add_argument('--num_data', type=int, default=306342)
+    args.add_argument('--shuffle_buffer', type=int, default=2048)
     args.add_argument('--train', type=bool, default=True)
-    args.add_argument('--checkpoint_path', type=str, default="./checkpoint/best_model.ckpt")
+    args.add_argument('--checkpoint_path', type=str, default="gs://chandradeep_data/best_model.ckpt")
     # args.add_argument('--dataset_path', type=str, default="/home/chan/data/augmented_dataset_300k.csv")
     # args.add_argument('--image_dir', type=str, default="/home/chan/data/augmented_images_300k/")
-    args.add_argument('--trainset_path', type=str, default="/home/chan/data/trainset_300k.record")
-    args.add_argument('--valset_path', type=str, default="/home/chan/data/valset_300k.record")
+    args.add_argument('--trainset_path', type=str, default="gs://chandradeep_data/trainset_300k.record")
+    args.add_argument('--valset_path', type=str, default="gs://chandradeep_data/valset_300k.record")
 
     config = args.parse_args()
 
@@ -67,34 +67,34 @@ if __name__ == "__main__":
     tf.config.experimental_connect_to_cluster(resolver)
     tf.tpu.experimental.initialize_tpu_system(resolver)
     strategy = tf.distribute.experimental.TPUStrategy(resolver)
-    tf.compat.v1.disable_eager_execution() 
+#    tf.compat.v1.disable_eager_execution() 
 
     # tpu_model = tf.contrib.tpu.keras_to_tpu_model( model,strategy=tf.contrib.tpu.TPUDistributionStrategy(tf.contrib.cluster_resolver.TPUClusterResolver(TPU_WORKER)))
     with strategy.scope():
         model = BlazeFace(config).build_model()
         opt = tf.keras.optimizers.Adam(learning_rate = config.learning_rate)
         model.compile(loss= custom_loss, optimizer=opt)
-        # early_stopping = tf.keras.callbacks.EarlyStopping(
-        #                 monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='min',
-        #                 baseline=None, restore_best_weights=True
-        #             )
+        early_stopping = tf.keras.callbacks.EarlyStopping(
+                        monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='min',
+                        baseline=None, restore_best_weights=True
+                    )
         
-        # checkpoint = tf.keras.callbacks.ModelCheckpoint(
-        #             config.checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
-        #             save_weights_only=True, mode='min', save_freq='epoch')
+       # checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        #            config.checkpoint_path, monitor='val_loss', verbose=1, save_best_only=True,
+         #           save_weights_only=False, mode='min', save_freq='epoch')
         
         train_gen = create_dataset(config.train_batch_size, config.trainset_path, config.shuffle_buffer)
         STEP_SIZE_TRAIN = int((0.95 * config.num_data) // config.train_batch_size)
         val_gen = create_dataset(config.val_batch_size, config.valset_path, config.shuffle_buffer)
         STEP_SIZE_VAL = int((0.05 * config.num_data) // config.val_batch_size)
 
-        model.fit(x=train_gen[0], y = train_gen[1], epochs = config.epochs, 
+        model.fit(x=train_gen, epochs = config.epochs, 
                                         steps_per_epoch = STEP_SIZE_TRAIN,
                                         validation_data = val_gen,
                                         validation_steps = STEP_SIZE_VAL,
-                                        # callbacks = [early_stopping, checkpoint],
+                                        callbacks = [early_stopping],
                                         verbose=1,
                                         shuffle=True,
                                         use_multiprocessing=False)
-
+        model.save("./model_300k.h5")
     # train(model, config)
