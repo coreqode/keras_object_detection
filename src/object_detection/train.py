92754f7a9e9037bcb1f68bb1fdd3801eef69f371
coreqode <chinu.deep100@gmail.com>
2020-04-14 14:17:12 +0530

    minor changes

--- src/object_detection/train.py
+++ src/object_detection/train.py
@@ -38,7 +38,7 @@ def train(model, config):
                                     verbose=1,
                                     shuffle=True,
                                     use_multiprocessing=False)
-    print(res.values)
+    # print(res.values)
 
 if __name__ == "__main__":
 
@@ -55,24 +55,46 @@ if __name__ == "__main__":
     args.add_argument('--checkpoint_path', type=str, default="./checkpoint/best_model.ckpt")
     # args.add_argument('--dataset_path', type=str, default="/home/chan/data/augmented_dataset_300k.csv")
     # args.add_argument('--image_dir', type=str, default="/home/chan/data/augmented_images_300k/")
-    args.add_argument('--trainset_path', type=str, default="gs://chandradeep_data/trainset_300k.record")
-    args.add_argument('--valset_path', type=str, default="/gs://chandradeep_data/valset_300k.record")
+    args.add_argument('--trainset_path', type=str, default="/home/chan/data/trainset_300k.record")
+    args.add_argument('--valset_path', type=str, default="/home/chan/data/valset_300k.record")
 
     config = args.parse_args()
 
     tf.keras.backend.clear_session()
-    TPU_WORKER = 'grpc://' + '10.12.97.242:8470'
+    TPU_WORKER = 'grpc://' + '10.108.85.226:8470'
     # tf.config.experimental_connect_to_cluster(TPU_WORKER)
     resolver = tf.distribute.cluster_resolver.TPUClusterResolver(TPU_WORKER)
     tf.config.experimental_connect_to_cluster(resolver)
     tf.tpu.experimental.initialize_tpu_system(resolver)
     strategy = tf.distribute.experimental.TPUStrategy(resolver)
-    #tf.compat.v1.disable_eager_execution() 
+    tf.compat.v1.disable_eager_execution() 
 
     # tpu_model = tf.contrib.tpu.keras_to_tpu_model( model,strategy=tf.contrib.tpu.TPUDistributionStrategy(tf.contrib.cluster_resolver.TPUClusterResolver(TPU_WORKER)))
     with strategy.scope():
         model = BlazeFace(config).build_model()
         opt = tf.keras.optimizers.Adam(learning_rate = config.learning_rate)
         model.compile(loss= custom_loss, optimizer=opt)
+        # early_stopping = tf.keras.callbacks.EarlyStopping(
+        #                 monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='min',
+        #                 baseline=None, restore_best_weights=True
+        #             )
+        
+        # checkpoint = tf.keras.callbacks.ModelCheckpoint(
+        #             config.checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False,
+        #             save_weights_only=True, mode='min', save_freq='epoch')
+        
+        train_gen = create_dataset(config.train_batch_size, config.trainset_path, config.shuffle_buffer)
+        STEP_SIZE_TRAIN = int((0.95 * config.num_data) // config.train_batch_size)
+        val_gen = create_dataset(config.val_batch_size, config.valset_path, config.shuffle_buffer)
+        STEP_SIZE_VAL = int((0.05 * config.num_data) // config.val_batch_size)
 
-    train(model, config)
+        model.fit(x=train_gen[0], y = train_gen[1], epochs = config.epochs, 
+                                        steps_per_epoch = STEP_SIZE_TRAIN,
+                                        validation_data = val_gen,
+                                        validation_steps = STEP_SIZE_VAL,
+                                        # callbacks = [early_stopping, checkpoint],
+                                        verbose=1,
+                                        shuffle=True,
+                                        use_multiprocessing=False)
+
+    # train(model, config)
